865ab9c18e9cf5f0bb74f8ed6af1e2a1af2970ba
qiaoqiao2 <1061522616@qq.com>
2019-01-26 22:03:20 +0800

    修改main

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] arg){
-        NotifyDriver notifyDriver = new NotifyDriver();
-        notifyDriver.notifyDriver();
-        BorrowDriver borrowDriver=new BorrowDriver();
-        borrowDriver.borrow();
+
 
         //方法调用控制器
         while(true){
-            System.out.println("请输入方法的编号(0退出/1借书/3读文件/21管理员创建和搜索用户/22管理员编辑书籍和用户/23管理员生成报告): ");
+            System.out.println("请输入方法的编号(0退出/1借书/3读文件/21管理员创建和搜索用户/22管理员编辑书籍和用户/23管理员生成报告/4用户修改个人信息): ");
             Scanner sc = new Scanner(System.in);
             int cmd = sc.nextInt();
             if(cmd == 0){
                 break;
             }
-            /*else if(cmd == 1){
+            else if(cmd == 1){
                 BorrowDriver borrowDriver=new BorrowDriver();
                 borrowDriver.borrow();
-            }*/
+            }
             else if(cmd == 3){
                 ReaderDriver readerDriver = new ReaderDriver();
                 readerDriver.ReadWordDriver();
@@ -44,6 +41,10 @@ public class Main {
                 AdminManageDriver adminManageDriver = new AdminManageDriver();
                 adminManageDriver.adminReportBorrowRecords();
                 adminManageDriver.adminReportOverduePayments();
+            }
+            else if(cmd == 4){
+                NotifyDriver notifyDriver = new NotifyDriver();
+                notifyDriver.notifyDriver();
             }
             else{
                 System.out.println("cannot find the function");
